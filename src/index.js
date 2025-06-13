d17a61081eb27f332345feb8b361135d33c6e288
pranjaltomar9 <tomarpranjal9@gmail.com>
2025-06-13 18:11:17 +0530

    api error handling and response

--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ dotenv.config({
 
 
 connectDB()
-
-
-
-
-
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at PORT ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed  !!!", err);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+// --------------------using trycatch-----------------------
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (err) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+
+
+
+
+// --------------------using promises-----------------------
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+export {asyncHandler}
